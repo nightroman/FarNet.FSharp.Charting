e8ca3a503875f04b4d6bd53c2f09d36fe9b77293
Roman Kuzmin <nightroman@gmail.com>
2025-09-29 06:18:29 +0000

    Build, test, SatelliteResourceLanguages

--- test/samples.test.ps1
+++ test/samples.test.ps1
@@ -1,11 +1,10 @@
 ﻿
 Set-StrictMode -Version 3
-$Testing = @{Test = 500; Timeout = 30000}
 
 foreach($item in Get-Item ..\samples\*.fsx) {
 	if ($item.Name -notmatch '^(?:Far|StockAndCandlestick|VanillaLine)') {
 		task $item.Name {
-			Start-Far @Testing "fs:exec file=..\samples\$($task.Name)"
+			Start-Far "fs:exec file=..\samples\$($task.Name)" -Exit 500, 30000
 		}
 	}
 }
